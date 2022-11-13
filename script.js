0711a372497c2e055eadc100cdab3bf8356e0fb2
Ne0n2018 <90716928+Ne0n2018@users.noreply.github.com>
2022-11-13 15:00:41 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -13,10 +13,14 @@
 // sum(); // введите два параметра
 
  let smartSum = function (a,b) {
-     if (typeof a == "number" && typeof b  == "number" ) {
-         return  a + b;
+     if (arguments.length == 2 ) {
+         if (typeof arguments[0] == "number" && typeof arguments[1] == "number") {
+            return a * b;
+         } else {
+            console.error;
+         }
      } else {
-         console.log("error!");
+         console.log("введите два числа");
      }
  }
  console.log(smartSum(4,77));
@@ -27,14 +31,23 @@
 
 //  2)Измените функцию "square" так, чтобы в случае ее вызова без аргумента генерировалась консольная ошибка (console.error):
 //  "Функция "square" не может быть вызвана без аргумента"
- 
- 
+
+//  let square = function (a) {
+//     if (a != undefined) {
+//         console.log(a * a);
+//     } else {
+//         console.log("error!");
+//     }
+//   }
+//   square(10);
+//   square();
+
 //  function square(a) {
 //    console.log(a * a)
 //  }
- 
+
 //  square(10) // 100
- 
+
 //  square()
 //  // ДО: NaN
 //  // ПОСЛЕ: Uncaught Error: Функция "square" не может быть вызвана без аргумента
@@ -50,6 +63,15 @@
    square();
 
 
+// let game = (a) => {
+//     let ran = Math.random () * 10;
+//     if (a == ran) {
+//         console.log("числа совпали");
+//     } else {
+//         console.log(`числа не совпалию Ваше число ${a}. загадагнное число ${ran}`);
+//     }
+// }
+// game(7);
 
 
 
@@ -57,7 +79,7 @@
 //   3) Создать функцию "угадай число". Она принимает число от 1 до 10 (обязательно проверить, что число не больше 10 и не меньше 0). Генерирует рандомное число от 1 до 10 и сравнивает с заданным числом.
 
 //    Если они совпали, то возвращает “Вы выиграли”, если нет - то “Вы не угадали, ваше число -  ...,  а выпало число ...”
-   
+
 //    Функция создания случайного числа уже была ранее в материалах, в задаче по созданию случайного цвета.
    
 //    Написать функцию в стрелочном синтаксисе.
@@ -82,8 +104,8 @@
 //  Например, запуск функции filterFor(arr, 5) дает результат [5,8]
 //  запуск функции filterFor(arr, 10) дает результат []
 //  запуск функции filterFor(arr, 3.11) дает результат [4,5,8]
- 
- 
+
+
  let filterFor = (arr, a) =>{
      let newArr = [];
      for (let i = 0; i < arr.length; i++) {
@@ -115,19 +137,27 @@
 
 // 6) Создайте функцию sumObjectValues, которая будет суммировать все значения свойств, которые являются числами. Сумму чисел необходимо вернуть из функции.
 //  Проверить работу функции можно на объекте:
- 
-//  const objectWithNumbers = {
-//    a: 10,
-//    b: 20,
-//    c: 'string',
-//    d: 12,
-//  }
-const objectWithNumbers = [ 10, 20, 'string', 12 ];
-let sum = 0;
-objectWithNumbers.forEach((item ,index) =>{
-    if (typeof objectWithNumbers[index] == "number") {
-        sum = sum + item;
+// const objectWithNumbers = {
+//     a: 10,
+//     b: 20,
+//     c: 'string',
+//     d: 12,
+//   }
+
+
+let sum = (objectWithNumbers) =>{
+    sum = 0;
+    for (const key in objectWithNumbers) {
+        if (typeof objectWithNumbers[key] == "number") {
+            sum = sum + objectWithNumbers[key];
+        }
     }
-    return sum;
-})
+};
+const objectWithNumbers = {
+    a: 10,
+    b: 20,
+    c: 'string',
+    d: 12,
+  }
+  sum(objectWithNumbers);
 console.log(sum);
